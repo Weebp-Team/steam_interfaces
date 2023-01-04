a7561a1f0dbefc0436df1ddcca352ebff997f8be
Daniil Detter <vudi600@gmail.com>
2023-01-04 22:14:13 +0700

    Add interface ISteamWebAPIUtil

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 :copyright: (c) 2022 Tarodictrl
 """
 
-version = '0.2.0'
+version = '0.2.1'
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
